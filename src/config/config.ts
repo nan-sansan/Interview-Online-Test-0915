48a9ebb231b5092143f99492f6fa385be5614576
DESKTOP-QEOMG0Q\NJ <a85124236@gmail.com>
2025-09-15 13:14:36 +0800

    mod: 修改主要config workRoute

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,12 +3,14 @@ import { List, LogIn, Send, UserPlus } from "lucide-react";
 export type RouteItem = (typeof workRoute)[number];
 
 export const workRoute = [
+  { path: "/login", display: "使用者登入", icon: LogIn, loginDisable: true },
   {
-    path: "/typeAccount",
-    display: "名稱顯示",
-    icon: Send,
+    path: "/register",
+    display: "員工登錄",
+    icon: UserPlus,
+    loginDisable: false,
   },
-  { path: "/register", display: "註冊", icon: UserPlus, loginDisable: true },
-  { path: "/login", display: "登入", icon: LogIn, loginDisable: true },
-  { path: "/userList", display: "用戶列表", icon: List, loginDisable: false },
+  { path: "/userList", display: "通訊錄列表", icon: List, loginDisable: false },
+  //TODO
+  { path: "/userList", display: "常用聯絡人", icon: Send },
 ];
